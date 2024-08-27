9599210ea2d8b6424bb5cd7c6b5f3c321f42928f
al-badarin <jamalalbadarin@gmail.com>
2024-08-27 21:11:08 +0300

    Update Navbar.jsx

--- grip-socks-becomepro/src/components/Navbar/Navbar.jsx
+++ grip-socks-becomepro/src/components/Navbar/Navbar.jsx
@@ -4,33 +4,41 @@ import { FaBars, FaTimes } from 'react-icons/fa';
 import logo from '../../assets/logo.png'; // Adjust the path according to your file structure
 
 const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleNavbar = () => {
-        setIsOpen(!isOpen);
-    };
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
 
-    return (
-        <header className={styles.navbar}>
-            <div className={styles.navContainer}>
-                <div className={styles.logoContainer}>
-                    <img src={logo} alt="BecomePro Logo" className={styles.logo} />
-                    <div className={styles.brandTitle}>BecomePro</div>
-                </div>
-                <div className={styles.toggleButton} onClick={toggleNavbar}>
-                    {isOpen ? <FaTimes /> : <FaBars />}
-                </div>
-                <nav className={`${styles.navLinks} ${isOpen ? styles.active : ''}`}>
-                    <ul>
-                        <li><a href="#home">Home</a></li>
-                        <li><a href="#features">Features</a></li>
-                        <li><a href="#pricing">Pricing</a></li>
-                        <li><a href="#order">Order Now</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-    );
+  return (
+    <header className={styles.navbar}>
+      <div className={styles.navContainer}>
+        <div className={styles.logoContainer}>
+          <img src={logo} alt="BecomePro Logo" className={styles.logo} />
+          <div className={styles.brandTitle}>BecomePro</div>
+        </div>
+        <div className={styles.toggleButton} onClick={toggleNavbar}>
+          {isOpen ? <FaTimes /> : <FaBars />}
+        </div>
+        <nav className={`${styles.navLinks} ${isOpen ? styles.active : ''}`}>
+          <ul>
+            <li>
+              <a href="#home">Начало</a>
+            </li>
+            <li>
+              <a href="#features">Характеристики</a>
+            </li>
+            <li>
+              <a href="#pricing">Цени</a>
+            </li>
+            <li>
+              <a href="#order">Поръчай сега</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;

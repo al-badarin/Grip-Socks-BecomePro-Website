8cb722ecfd877472ff056e2b301c1dfd00b50c5f
al-badarin <jamalalbadarin@gmail.com>
2024-09-01 15:43:30 +0300

    add promotion to hero

--- grip-socks-becomepro/src/components/Hero/Hero.jsx
+++ grip-socks-becomepro/src/components/Hero/Hero.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Link } from 'react-scroll';
-
 import styles from './Hero.module.css';
 
 const Hero = () => {
   return (
     <section id="home" className={styles.hero}>
+      <div className={styles.promoBanner}>
+        <Link
+          to="pricing"
+          smooth={true}
+          duration={500}
+          className={styles.promoLink}
+        >
+          💥 Специална оферта: Купи 2 и получи 1 <span className='green'>БЕЗПЛАТНО</span>! Валидна до 08.09.2024 💥
+        </Link>
+      </div>
       <div className={styles.heroContent}>
         <h1 className={styles.heroTitle}>Grip Socks BecomePro</h1>
         <p className={styles.heroSubtitle}>
